41a9c6a0c1e61f290db5c5e46ee22223582e566d
Ursa Americanus Kermodei <venantius@gmail.com>
2017-01-08 13:59:25 -0800

    [ultra] Bump version to 0.5.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject venantius/ultra "0.5.0"
+(defproject venantius/ultra "0.5.1-SNAPSHOT"
   :description "Ultra: A Leiningen plugin for a superior development environment"
   :url "http://github.com/venantius/ultra"
   :license {:name "Eclipse Public License"
