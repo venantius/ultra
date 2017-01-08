5d0c3cb857e37b28e40a51b1bae8d2ddfd242c96
Ursa Americanus Kermodei <venantius@gmail.com>
2017-01-08 14:12:15 -0800

    [ultra] Move clojure dependency to :dev profile
    This moves the org.clojure/clojure dependency out of the plugin's main
    list of dependencies and into its :dev profile. This is necessary
    because Ultra will otherwise attempt to rewrite project maps,
    potentially forcing an unwanted Clojure version on the end user.
    
    Unfortunately, Ultra will still engage in what could be considered a
    slightly unfriendly behavior here with regard to potential exclusions of
    Ultra's dependencies -- I'm feeling a little too lazy to do very much
    about that for now, but might want to address the issue in the future.

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :url "http://github.com/venantius/ultra"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.nrepl "0.2.12"]
+  :dependencies [[org.clojure/tools.nrepl "0.2.12"]
 
                  [grimradical/clj-semver "0.3.0" :exclusions [org.clojure/clojure]]
                  [io.aviso/pretty "0.1.30"]
@@ -13,7 +12,8 @@
                  [org.clojars.brenton/google-diff-match-patch "0.1"]
                  [robert/hooke "1.3.0"]
                  [venantius/glow "0.1.4" :exclusions [hiccup garden]]]
-  :profiles {:dev {:dependencies [[circleci/bond "0.2.9"]]}}
+  :profiles {:dev {:dependencies [[circleci/bond "0.2.9"]
+                                  [org.clojure/clojure "1.8.0"]]}}
   :test-selectors {:default (complement :demo)
                    :demo :demo}
   :min-lein-version "2.5.2"
