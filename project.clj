dbe5915a84faa3d90ac60aea429ce36a457e2df4
David Jarvis <venantius@gmail.com>
2018-02-28 10:08:42 +0000

    [deps] Update Glow and Pyro dependencies.

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
   :dependencies [[org.clojure/tools.nrepl "0.2.12"]
 
                  [grimradical/clj-semver "0.3.0" :exclusions [org.clojure/clojure]]
-                 [venantius/pyro "0.1.0"]
+                 [venantius/pyro "0.1.1"]
                  [mvxcvi/whidbey "1.3.0"]
                  [mvxcvi/puget "1.0.1"]
                  [org.clojars.brenton/google-diff-match-patch "0.1"]
                  [robert/hooke "1.3.0"]
-                 [venantius/glow "0.1.4" :exclusions [hiccup garden]]]
+                 [venantius/glow "0.1.5" :exclusions [hiccup garden]]]
   :profiles {:dev {:dependencies [[circleci/bond "0.2.9"]
                                   [org.clojure/clojure "1.8.0"]]}}
   :test-selectors {:default (complement :demo)
